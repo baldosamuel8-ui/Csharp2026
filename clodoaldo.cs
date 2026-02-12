bbe23f5661b0f2b628fb03e22af541276bb81742
baldosamuel8-ui <baldosamuel8@gmail.com>
2026-02-11 21:22:40 -0300

    Primeiro Codigo no Curso de C#

---
+++ clodoaldo.cs
@@ -0,0 +1,9 @@
+using System; //Biblioteca do C# (system)para ua o sistema do console
+
+    public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Oi, Senai");
+    }
+}
