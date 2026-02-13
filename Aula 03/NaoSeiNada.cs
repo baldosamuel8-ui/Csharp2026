8193a946ef1b2b3d50f473ee63db1697b9e12145
baldosamuel8-ui <baldosamuel8@gmail.com>
2026-02-13 19:47:51 -0300

    Aula 03

---
+++ Aula 03/NaoSeiNada.cs
@@ -0,0 +1,7 @@
+double x, y, media;
+    Console.WriteLine("Digite o Primeiro Numero; ");
+x =double.Parse(Console.ReadLine());
+    Console.WriteLine("Digite o Segundo Numero: ");
+y =double.Parse(Console.ReadLine());
+    media = (x+y)/2.0;
+    Console.WriteLine("A Media = " + media);
