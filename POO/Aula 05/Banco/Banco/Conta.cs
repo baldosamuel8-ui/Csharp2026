097e544c2cf1352b542c9f397a0932174ef77f7a
baldosamuel8-ui <baldosamuel8@gmail.com>
2026-03-25 21:55:09 -0300

    Aula 05

---
+++ POO/Aula 05/Banco/Banco/Conta.cs
@@ -0,0 +1,38 @@
+﻿namespace Banco
+{
+    internal class Conta
+    {
+        //Campos ou Atributos
+        private int numero;
+        private string titular;
+        private double saldo;
+
+        //Construtor
+        public Conta(int numero, string titular)
+        {
+            this.numero = numero;
+            this.titular = titular;
+            saldo = 0;
+        }
+
+        public Conta(int numero, string titular, double saldo): this(numero, titular)//Reaproveitamento de Construtor anterior
+        {
+            this.saldo = saldo;
+        }
+
+        //Metodos
+        public void Deposito(double quantia)
+        {
+            saldo += quantia;
+        }
+        public void Saque(double quantia)
+        {
+            saldo -= quantia;
+        }
+        public void Dados()
+        {
+            Console.WriteLine("Dados da conta cadastrada: ");
+            Console.Write($"\tTitular: {titular}\n" + $"\t Numero da conta: {numero}\n" + $"\tSaldo da conta: {saldo}\n");
+        }
+    }
+}
---
+++ POO/Aula 05/Banco/Banco/Program.cs
@@ -0,0 +1,43 @@
+﻿using static System.Console;
+using Banco;
+using static System.Globalization.CultureInfo;
+
+try {
+
+    Title = "Abertura de conta";
+    Conta conta;
+
+    WriteLine("Abertuta de conta - Nika Investment");
+    Write("Entre com um número de conta: ");
+    int numero = int.Parse(ReadLine());
+    Write("Entre com o nome do Titular: ");
+    string nome = ReadLine();
+    Write("Deseja fazer deposito inicial (s/n): ");
+    char resposta = char.Parse(ReadLine().ToLower());
+
+    if ((resposta) == 's')
+    {
+        Write("Entre com valor de depósito inicial: R$ ");
+        double deposito = double.Parse(ReadLine());
+
+        conta = new Conta(numero, nome, deposito);
+    }
+    else
+    {
+        conta = new Conta(numero, nome); 
+    }
+
+    conta.Dados();
+    WriteLine("Entre com um valor para depósito: R$ ");
+    double quantia = double.Parse(ReadLine());
+    conta.Deposito(quantia);
+    WriteLine("Entre com um valor para saque: R$ ");
+    quantia = double.Parse(ReadLine());
+    conta.Saque(quantia);
+
+    ReadKey();
+}
+catch(Exception)
+{
+    WriteLine("Escreva um valor Valido!");
+}
