651a8a4370d8e6ea5ef53546748ba11e727d5ce2
baldosamuel8-ui <baldosamuel8@gmail.com>
2026-04-24 20:45:47 -0300

    Aula 05 - Banco de Dados

---
+++ Banco de Dados/Aula 05/MasterBanco/MasterBanco/Dados/Consultas/SQLQuery3.sql
@@ -0,0 +1,8 @@
+--Inserir dados de teste p/ a tabela--
+
+INSERT INTO Contas(Titular,Numero_Da_Conta,Saldo)VALUES
+('João Silva', 1001, 5000.00),
+('Maria Santos', 1002, 1500.00),
+('Pedro Oliveira', 1003, 250.00),
+('Elma Maria', 1004, 75000.00);
+GO
